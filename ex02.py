df387dd88ade5406d1e697812d9d48b5e942ee11
prakashpy <prakash.py@outlook.com>
2017-11-19 13:40:42 -0500

    ex02.py

---
+++ ex02.py
@@ -0,0 +1,14 @@
+#Ex 2
+#Define a function that computes the length of a given list or string.
+#(It is true that Python has the len() function built in, but writing it yourself is nevertheless a good exercise.)
+
+def length(string):
+    count = 0
+    for i in string:
+        count += 1
+    return count
+
+
+#if __name__ == "__main__":
+print length("test string")
+print length([1,2,3])
