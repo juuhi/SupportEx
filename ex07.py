b129a151459f8dffd6db11c7c0a56be387455397
prakashpy <prakash.py@outlook.com>
2017-11-19 13:58:44 -0500

    ex07.py

---
+++ ex07.py
@@ -0,0 +1,12 @@
+#Ex 7
+#Define a procedure histogram() that takes a list of integers and prints a histogram to the screen.
+#For example, histogram([4, 9, 7]) should print the following:
+#****
+#*********
+#*******
+
+def histogram(inputList):
+    for i in range(len(inputList)):
+        print (inputList[i]*'*')
+
+histogram([4,9,7])
